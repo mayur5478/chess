309661a5ba0f372bfff70c2acf43ae287c5333a9
Mayur Lunawat <lunawatmayur8@gmail.com>
2015-05-24 14:26:29 +0530

    Chess

---
+++ Chess/config/showBoard.php
@@ -0,0 +1,11 @@
+<?php 
+$dbc = mysqli_connect('localhost:3308','root','root','ChessDb');
+$id = filter_input(INPUT_GET,'QuestionId');
+$query = "select Position from puzzle_question where id = $id";
+$r = mysqli_query($dbc,$query);
+$position;
+while($row = mysqli_fetch_array($r)){
+	$position = $row[0];
+}
+echo $position;
+?>
---
+++ Chess/template/question.php
@@ -0,0 +1,21 @@
+<div class="container">
+				<div id = 'board'  style="width: 400px"> </div>
+			</div>
+			<div id="sidePanel">
+				<label>Question</label>
+				<input type="text"  id="question" required="true">
+				<label id="levelLabel">Level</label>
+				<select id= "level">
+				  <option >1</option>
+				  <option >2</option>
+				  <option >3</option>
+				  <option >4</option>
+				</select>
+				<label id="firstMoveLabel">First Move</label>
+				<select id= "firstMove">
+				  <option >w</option>
+				  <option >b</option>
+				</select>
+				<button type="submit" class="btn btn-back" id="saveAndNext"  >Save And Next <i class="fa fa-floppy-o"></i></button>
+				<button type="submit" class="btn btn-back" id="reset">Reset <i class="fa fa-refresh"></i> </button>
+	  		</div>
